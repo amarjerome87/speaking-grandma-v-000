6b2384c20b0435d2fbb182fc541a68ad280d5bb9
Amar Chatman <amarjerome@gmail.com>
2018-02-08 00:03:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,9 +6,9 @@ def speak_to_grandma (phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return  "I LOVE YOU TOO PUMPKIN!"
+  end 
 end
 end
-end 
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
